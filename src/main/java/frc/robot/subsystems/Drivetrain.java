dabe06b21577517668138a91e41ef6464baa5703
Pidud <TsuchitoriJoel@gmail.com>
2019-01-14 14:56:06 -1000

    added gyro turn code to test

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -10,6 +10,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -18,51 +19,38 @@ import frc.robot.RobotMap;
 import frc.robot.commands.OperateMecanumDrive;
 import frc.robot.commands.OperateTankDrive;
 import frc.util.SparkWrapper;
-//import frc.util.Clipper;
 import frc.util.SparkWrapper;
 
 public class Drivetrain extends Subsystem {
 
   private CANSparkMax _frontLeftMain;
-  //private TalonSRX _frontLeftSlave;
-
   private CANSparkMax _frontRightMain;
-  //private TalonSRX _frontRightSlave;
-
   private CANSparkMax _backLeftMain;
-  //private TalonSRX _backLeftSlave;
-
   private CANSparkMax _backRightMain;
-  //private TalonSRX _backRightSlave;
+
+  private double kP = 0;
+  private double kI = 0;
+  private double kD = 0;
+  private double kIZone = 0;
+  private double kFF = 0;
+  private final int PID_TURN = 0;
+
 
   private MecanumDrive drive;
   
   public void init(){
     _frontLeftMain = new CANSparkMax(RobotMap.frontLeftMain, MotorType.kBrushless);
-    //_frontLeftSlave = new TalonSRX(RobotMap.frontLeftSlave);
-    //_frontLeftSlave.follow(_frontLeftMain);
     _frontLeftMain.setInverted(true);
-    //_frontLeftSlave.setInverted(true);
+    _frontLeftMain.getPIDController().setP(kP, PID_TURN);
 
     _frontRightMain =  new CANSparkMax(RobotMap.frontRightMain, MotorType.kBrushless);
-    //_frontRightSlave = new TalonSRX(RobotMap.frontRightSlave);
-    //_frontRightSlave.follow(_frontRightMain);
     _frontRightMain.setInverted(true);
-    //_frontRightSlave.setInverted(true);
-    
-
 
     _backLeftMain = new CANSparkMax(RobotMap.backLeftMain, MotorType.kBrushless);
-    //_backLeftSlave = new TalonSRX(RobotMap.backLeftSlave);
-    //_backLeftSlave.follow(_backLeftMain);
     _backLeftMain.setInverted(true);
-    //_backLeftSlave.setInverted(true);
     
     _backRightMain = new CANSparkMax(RobotMap.backRightMain, MotorType.kBrushless);
-    //_backRightSlave = new TalonSRX(RobotMap.backRightSlave);
-    //_backRightSlave.follow(_backRightMain);
     _backRightMain.setInverted(true);
-    //_backRightSlave.setInverted(true);
    
 
     drive = new MecanumDrive(
@@ -82,6 +70,15 @@ public class Drivetrain extends Subsystem {
 
   public void setMecanum(double x, double y, double rotation, double gyroAngle){
     drive.driveCartesian(x, y, rotation, gyroAngle);
+  }
+
+  public void setTurn(double target, double gyroAngle){
+    _frontLeftMain.pidWrite(gyroAngle / 180);
+    _frontLeftMain.getPIDController().setReference(target * 180, ControlType.kDutyCycle, PID_TURN);
+
+    _backLeftMain.set(_frontLeftMain.get());
+    _backRightMain.set(-_frontLeftMain.get());
+    _frontRightMain.set(-_frontLeftMain.get());
   }
 
   @Override
