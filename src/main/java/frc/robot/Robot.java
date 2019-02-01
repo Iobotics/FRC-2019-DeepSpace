e5446102f69f39f6341c26cd90b87e4437bbf808
Pidud <TsuchitoriJoel@gmail.com>
2019-01-31 17:07:07 -1000

    Fixed PID drive so it would travel in proper direction on both sides

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -41,7 +41,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
 
     Compressor _compressor = new Compressor();
-    _compressor.start();
+    _compressor.stop();
 
     CommandBase.init();
     // chooser.addOption("My Auto", new MyAutoCommand());
@@ -95,7 +95,7 @@ public class Robot extends TimedRobot {
      * = new MyAutoCommand(); break; case "Default Auto": default:
      * autonomousCommand = new ExampleCommand(); break; }
      */
-    m_autonomousCommand = new AutoTurn(90);
+    m_autonomousCommand = new AutoDrive(10);
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.start();
@@ -130,29 +130,8 @@ public class Robot extends TimedRobot {
     
     Scheduler.getInstance().run();
     SmartDashboard.putBoolean("Calibraing", CommandBase.navSensor.isCalibrating());
-    /* 
-    SmartDashboard.putData("DriveTrain", CommandBase.drivetrain);
-    SmartDashboard.putNumber("kP Drive", CommandBase.drivetrain.getP());
-    SmartDashboard.putNumber("kI Drive", CommandBase.drivetrain.getI());
-    SmartDashboard.putNumber("kD Drive", CommandBase.drivetrain.getD());
-    SmartDashboard.putNumber("kFF Drive", CommandBase.drivetrain.getFF());
-    SmartDashboard.putNumber("kIzone Drive", CommandBase.drivetrain.getIzone());
 
-     double p = SmartDashboard.getNumber("kP Drive", 0);
-     if (p != CommandBase.drivetrain.getP()){CommandBase.drivetrain.setP(p);}
-
-     double  i= SmartDashboard.getNumber("kI Drive", 0);
-     if (i != CommandBase.drivetrain.getI()){CommandBase.drivetrain.setI(i);}
-
-     double d = SmartDashboard.getNumber("kD Drive", 0);
-     if (d != CommandBase.drivetrain.getD()){CommandBase.drivetrain.setD(d);}
-
-     double ff = SmartDashboard.getNumber("kFF Drive", 0);
-     if (ff != CommandBase.drivetrain.getFF()){CommandBase.drivetrain.setFF(ff);}
-
-     double iZone = SmartDashboard.getNumber("kIzone Drive", 0);
-     if (iZone != CommandBase.drivetrain.getIzone()){CommandBase.drivetrain.setIzone(iZone );}
-     */
+     SmartDashboard.putNumber("encoder", CommandBase.drivetrain.getFrontRightPosition());
 
   }
 
