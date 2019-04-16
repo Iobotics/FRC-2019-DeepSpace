dce643e5a743220a3029fadc7788f35a441b55e2
Joel Tsuchitori <TsuchitoriJoel@gmail.com>
2019-04-15 15:32:54 -1000

    Redid Controls for Limlight and CameraServo

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -38,7 +38,7 @@ import frc.robot.commands.Drivetrain.AutoTurn;
 //import frc.robot.commands.hatch.OpenHook;
 //import frc.robot.commands.hatch.RetractHatch;
 //import frc.robot.commands.hatch.ToggleHatch;
-import frc.robot.commands.hatch.ToggleHook;
+import frc.robot.commands.Hatch.ToggleHook;
 import frc.robot.commands.Intake.IntakeBall;
 import frc.robot.commands.Intake.RunChassisIntake;
 import frc.robot.commands.Intake.SetIntakeVelocity;
@@ -60,7 +60,6 @@ import frc.robot.commands.Shooter.StopShooterArm;
 public class OI {
 
   private static boolean controllerEnabled = true; // TODO- Ask what default is
-  private static final int CAMERABUTTON = 2;
 
   private final Joystick _lStick = new Joystick(0);
   private final Joystick _rStick = new Joystick(1);
@@ -70,8 +69,8 @@ public class OI {
   // Intake Buttons
   private final JoystickButton intakeBall = new JoystickButton(_rStick, 1); // Right Trigger
   private final JoystickButton outtakeBall = new JoystickButton(_lStick, 1); // Left Trigger 
-  //private final JoystickButton runIntake = new JoystickButton(_controller, 1);
-  private final JoystickButton velocityIntake = new JoystickButton(_controller, 9);
+  private final JoystickButton runIntake = new JoystickButton(_controller, 1);
+  private final JoystickButton velocityIntake = new JoystickButton(_controller, 5);
 
   // Shooter Buttons
   private final JoystickButton positionShooterFirstLevel = new JoystickButton(_controller, 2);
@@ -90,20 +89,16 @@ public class OI {
   private final JoystickButton toggleZoneTwoBack = new JoystickButton(_lStick, 4);
   //private final JoystickButton autoZone3 = new JoystickButton(_rStick, 2);
 
-  private final JoystickButton cameraAutoCargo = new JoystickButton(_rStick, CAMERABUTTON);
-  private final JoystickButton cameraAutoRocket = new JoystickButton(_lStick, CAMERABUTTON);
-  private final JoystickButton rotateCamera = new JoystickButton(_lStick, 5); 
+  private final JoystickButton cameraAutoCargo = new JoystickButton(_rStick, 2);
+  private final JoystickButton cameraAutoRocket = new JoystickButton(_lStick, 2);
+  private final JoystickButton rotateCamera = new JoystickButton(_controller, 10);
+  private final JoystickButton rotateLimelight = new JoystickButton(_controller, 9); 
 
   private final JoystickButton enableController = new JoystickButton(_lStick, 10); // TODO- Ask which button
 
   private final JoystickButton gotoHabitat3 = new JoystickButton(_rStick, 10);
 
   public final JoystickButton ledSwitch = new JoystickButton(_rStick, 9);
-
-  //public final JoystickButton testAutoTurn = new JoystickButton(_rStick, 11);
-  public final JoystickButton shooterCargoOnly = new JoystickButton(_lStick, 8);
-  public final JoystickButton shooterRocketOnly = new JoystickButton(_lStick, 9);
-
 
   public OI(){
 
@@ -121,7 +116,7 @@ public class OI {
     });
     intakeBall.whenReleased(new StopIntakeBall());
 
-    /*runIntake.whenPressed(new ConditionalCommand(new RunChassisIntake()){
+    runIntake.whenPressed(new ConditionalCommand(new RunChassisIntake()){
       @Override
       protected boolean condition() {
         return controllerEnabled;
@@ -132,7 +127,7 @@ public class OI {
       protected boolean condition() {
         return controllerEnabled;
       }
-    });*/
+    });
 
     positionShooterFirstLevel.whenPressed(new ConditionalCommand(new PositionFirstLevel()){
       @Override
@@ -200,25 +195,17 @@ public class OI {
 
     cameraAutoCargo.whenPressed(new CameraAssist(false));
     cameraAutoRocket.whenPressed(new CameraAssist(true));
-    rotateCamera.whenPressed(new RotateLimelight());
+    
+    rotateCamera.whenPressed(new RotateCamera());
+    rotateLimelight.whenPressed(new RotateLimelight());
 
     velocityIntake.whenPressed(new SetIntakeVelocity());
     velocityIntake.whenReleased(new StopIntakeVelocity());
 
     enableController.whenPressed(new EnableController(!controllerEnabled));
 
-    //testAutoTurn.whenPressed(new TurnToTarget());
-    
     gotoHabitat3.whenPressed(new GoToHab3());
     gotoHabitat3.whenReleased(new ExitHab3());
-    //gotoHabitat3.toggleWhenPressed(new GoToHab3());
-
-
-    shooterCargoOnly.whenPressed(new HoldShooterPos(Constants.cargoShipAngle));
-    shooterCargoOnly.whenReleased(new SetShooterPos(Constants.shooterHome));
-
-    shooterRocketOnly.whenPressed(new HoldShooterPos(Constants.firstLevelAngle));
-    shooterRocketOnly.whenReleased(new SetShooterPos(Constants.shooterHome));
   }
 
 
@@ -267,7 +254,7 @@ public class OI {
 
   public boolean getCameraButton()
   {
-    return _lStick.getRawButton(CAMERABUTTON) || _rStick.getRawButton(CAMERABUTTON);
+    return _lStick.getRawButton(2) || _rStick.getRawButton(2);
   }
 
   public boolean getControllerButtons()
